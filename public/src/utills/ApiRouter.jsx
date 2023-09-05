133f0e4e3d3577b566548bd8f31ddbe103842fa7
SUBROTOKOIRY <kumarsubrato1103@gmail.com>
2023-09-06 01:26:41 +0530

    hosted the backend and changed the host name

--- public/src/utills/ApiRouter.jsx
+++ public/src/utills/ApiRouter.jsx
@@ -1,4 +1,4 @@
-const host = 'http://localhost:5000'
+const host = 'https://subroto-8wob.onrender.com'
 const registerRoute = `${host}/api/auth/Register`
 const loginRoute = `${host}/api/auth/login`
 const setProfilePictureRoute=`${host}/api/auth/setProfilePicture`
